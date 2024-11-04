00dfd7dd96d1686c5f77ef5457b9bda309e80a1c
Sean Park <jungbeanshaanprk@gmail.com>
2024-11-04 13:39:41 -0800

    Added Vue

---
+++ sandbox/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  export default {
+    data() {
+      return {
+        message: 'It works.',
+        count: 0
+      }
+    },
+    methods: {
+      incrementCount() {
+        this.count++
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>Counter</h1>
+    <p>{{ count }}</p>
+    <button v-on:click="incrementCount">Increment Count</button>
+    <hr/>
+</template>
