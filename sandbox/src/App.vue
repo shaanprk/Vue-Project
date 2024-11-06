fe1011a4e0e5a3f00092304d354cc91b8e95c361
Sean Park <jungbeanshaanprk@gmail.com>
2024-11-05 21:09:12 -0800

    Changes

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,72 +1,13 @@
 <script>
-  export default {
-    data: () => ({
-          message: 'The order of Straw Hat Pirates',
-          strawhats: [
-            {
-              name: "Luffy",
-              bounty: 3000000000
-            },
-            {
-              name: "Zoro",
-              bounty: 1111000000
-            },
-            {
-              name: "Nami",
-              bounty: 366000000
-            },
-            {
-              name: "Usopp",
-              bounty: 500000000
-            },
-            {
-              name: "Sanji",
-              bounty: 1032000000
-            },
-            {
-              name: "Chopper",
-              bounty: 1000
-            },
-            {
-              name: "Robin",
-              bounty: 930000000
-            },
-            {
-              name: "Franky",
-              bounty: 394000000
-            },
-            {
-              name: "Brook",
-              bounty: 383000000
-            },
-            {
-              name: "Jinbe",
-              bounty: 1100000000
-            }
-          ],
-          favoriteList: []
-        }),
-        methods: {
-          addToFavorites(character) {
-            this.favoriteList.push(character)
-          }
-        }
+import StrawhatPirates from "./components/StrawhatPirates.vue";
+
+export default {
+  components: {
+    StrawhatPirates
   }
+}
 </script>
 
 <template>
-  <h2>Straw Hat Pirates</h2>
-  <ol>
-    <li v-for="(character, index) in strawhats" :key="`character-${index}`">
-      {{ character.name }} - {{ character.bounty }}
-      <button @click="addToFavorites(`${character.name} - ${character.bounty}`)">
-        Favorite
-      </button>
-    </li>
-  </ol>
-
-  <h2>Favorite Straw Hat Pirates</h2>
-  <ol>
-    <li v-for="(favchar, index) in favoriteList" :key="`favchar-${index}`">{{ favchar }}</li>
-  </ol>
+  <StrawhatPirates />
 </template>
---
+++ sandbox/src/components/StrawhatPirates.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  data: () => ({
+      message: 'The order of Straw Hat Pirates',
+      strawhats: [
+        { name: "Luffy", bounty: 3000000000 },
+        { name: "Zoro", bounty: 1111000000 },
+        { name: "Nami", bounty: 366000000},
+        { name: "Usopp", bounty: 500000000 },
+        { name: "Sanji", bounty: 1032000000 },
+        { name: "Chopper", bounty: 1000 },
+        { name: "Robin", bounty: 930000000 },
+        { name: "Franky", bounty: 394000000 },
+        { name: "Brook", bounty: 383000000 },
+        { name: "Jinbe", bounty: 1100000000 }
+      ],
+      favoriteList: []
+  }),
+  methods: {
+    addToFavorites(character) {
+      this.favoriteList.push(character)
+    }
+  }
+};
+</script>
+
+<template>
+  <h2>Straw Hat Pirates</h2>
+  <ol>
+      <li v-for="(character, index) in strawhats" :key="`character-${index}`">
+        {{ character.name }} - {{ character.bounty }}
+        <button @click="addToFavorites(`${character.name} - ${character.bounty}`)">
+          Favorite
+        </button>
+      </li>
+  </ol>
+
+  <h2>Favorite Straw Hat Pirates</h2>
+  <ol>
+    <li v-for="(favchar, index) in favoriteList" :key="`favchar-${index}`">{{ favchar }}</li>
+  </ol>
+</template>
